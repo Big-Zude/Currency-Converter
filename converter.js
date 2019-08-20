efb4c4d69020caee9eb35ae5c92a78d618839d62
Big-Zude <zudemwango@gmail.com>
2019-08-20 12:15:07 +0200

    updated file

--- converter.js
+++ converter.js
@@ -1,6 +1,6 @@
 //exchange Rates:http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1
 //rest countries:https://restcountries.eu/rest/v2/currency/{currency}
-//apikey:5c3bd933236a15766310c6853bcebecb
+
 
 const axios=require('axios')
 const getExchangeRate = async (fromCurrency, toCurrency)=>{
