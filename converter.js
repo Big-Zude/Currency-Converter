786fefa9ea5d7a9cc544e65235bde1064243fd65
Big-Zude <zudemwango@gmail.com>
2019-08-20 10:19:37 +0200

    updated file

--- converter.js
+++ converter.js
@@ -8,10 +8,27 @@ const getExchangeRate = async (fromCurrency, toCurrency)=>{
     const rate=response.data.rates
     const euro=1/rate[fromCurrency]
     const exchangeRate=euro*rate[toCurrency]
+    if(isNaN(exchangeRate)){
+            throw new Error(`unable to get currency ${fromCurrency}`)
+    }
     return exchangeRate
 }
 
 const getCountries=async(toCurrency)=>{
     const response =await axios.get(`https://restcountries.eu/rest/v2/currency/${toCurrency}`)
-    console.log(response)
+    return response.data.map(country=>country.name);
 }
+
+const convertCurrency=async(fromCurrency,toCurrency,amount)=>{
+    const exchangeRate= await getExchangeRate(fromCurrency,toCurrency)
+    const countries=await getCountries(toCurrency)
+    const convertedAmount= (amount*exchangeRate).toFixed(2)
+    return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
+}
+
+convertCurrency('USD','GBP',30)
+    .then((message)=>{
+        console.log(message)
+    }).catch((error)=>{
+        console.log(error.message)
+    })
